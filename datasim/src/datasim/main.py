a52dc4a110ee0e6fc65299053b39b8134a8d6919
Pavel Popov <pashafizmat1@gmail.com>
2024-11-06 22:41:33 +0300

    dir layout rebuild

---
+++ datasim/src/datasim/main.py
@@ -0,0 +1,13 @@
+from .core import DataSimulator
+from tools import messages
+import time
+import os
+
+def main(): 
+    while True:
+        print("ЫasdЫ")
+        print(os.environ.get("PYTHONDONTWRITEBYTECODE"))
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
--- iot_proto_message/__init__.py
+++
@@ -1,3 +0,0 @@
-# iot_proto_message/__init__.py
-
-from .data_message_pb2 import Batch
