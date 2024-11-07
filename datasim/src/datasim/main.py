1af4699e285c66c581af7d5b7cbcd75b8a94e386
Pavel Popov <pashafizmat1@gmail.com>
2024-11-07 14:41:14 +0300

    signal handling

--- datasim/src/datasim/main.py
+++ datasim/src/datasim/main.py
@@ -1,41 +1,50 @@
 from proto import messages_pb2
 import asyncio
 import random
-
+import signal
 import logging
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# Логирование
-logging.info("Это информационное сообщение.")
-#logging.error("Это сообщение об ошибке.")
 
 class DataSimulator:
     def __init__(self, num_devices: int, frequency: float):
         self.num_devices = num_devices
         self.frequency = frequency
+        self.tasks = []
+        self.stop_event = asyncio.Event()
 
     async def generate_message(self, device_id: int):
-        while True:
-            logging.info("Это информационное сообщение.")
-            message = f"{device_id} привет"
-            print(message)
-            await asyncio.sleep(1 / self.frequency)
+        try:
+            while not self.stop_event.is_set():
+                message = f"Device {device_id} sending message."
+                logging.info(message)
+                await asyncio.sleep(1 / self.frequency)
+        finally:
+            logging.info(f"Task stopped for device {device_id}.")
+
+    def stop(self):
+        self.stop_event.set()
 
     async def start(self):
-        tasks = []
         for device_id in range(1, self.num_devices + 1):
-            tasks.append(self.generate_message(device_id))
+            self.tasks.append(self.generate_message(device_id))
 
-        await asyncio.gather(*tasks)
+        await asyncio.gather(*self.tasks)
 
 
 def main():
     num_devices = 5
-    frequency = 2
+    frequency = 1
     generator = DataSimulator(num_devices, frequency)
-    asyncio.run(generator.start())
+
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGTERM, generator.stop)
+    loop.add_signal_handler(signal.SIGINT, generator.stop)
+
+    try:
+        loop.run_until_complete(generator.start())
+    finally:
+        loop.close()
 
 if __name__ == "__main__":
     main()
